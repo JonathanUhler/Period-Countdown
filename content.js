0f716dcbd9cb51cd893bba52429a4e9bf1334699
Jonathan Uhler <jonathan@uhler.com>
2020-08-15 13:39:36 -0700

    worked canvas and js, version/name are now correct

---
+++ content.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('Periods');
+var context = canvas.getContext('2d');
+
+context.fillStyle = 'black';
+context.font = "30px Arial";
+context.fillText("test", 200, 200)
