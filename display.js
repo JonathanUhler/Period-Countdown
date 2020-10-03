8d5148b7be194a0d1dd672b5ba12db0edbc1ea10
Jonathan Uhler <jonathan@uhler.com>
2020-10-03 09:18:57 -0700

    implemented Calendar.js data

--- display.js
+++ display.js
@@ -5,7 +5,7 @@
 "use strict";
 
 // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-// Copyright 2020 Mike Uhler and Jonathan Uhler
+// Copyright 2020 Jonathan Uhler and Mike Uhler
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -26,44 +26,128 @@
 // IN THE SOFTWARE.
 // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
+// Canvas and context properties
+var canvas = document.getElementById('Periods');
+var context = canvas.getContext('2d');
 
-// =============================================================================
-// enableTimer
-//
-// ***SUBJECT TO CHANGE: A function that controls the renew timer (gathers new 
-// information and displays it on the Chrome extention).
-//
-// Arguments:
-//
-// enableTimer takes no arguments
-// =============================================================================
-enableTimer();
-function enableTimer () {
-    setInterval(function () {
-        gatherDayInfo();
-    }, 10)
+
+// Create a new instance of the Calendar class (with all information for current classes)
+calendar = new Calendar();
+
+
+// Text information variables
+var textPos = {
+    x: 10,
+    y: 35,
+    xOffset: 0,
+    yOffset: 70,
+    timeSize: "59px Arial",
+    nameSize: "35px Arial"
+}
+// Set common color for all text
+context.fillStyle = 'black';
+
+
+// The following variable lets us force a date and time to see the result.
+// If it is set to null, the current date/time is used
+// let lookupDateTime = "2020-10-03T09:50:00";
+let lookupDateTime = null;
+
+let eDate;
+
+if (lookupDateTime === null) {
+eDate = new Date(); // If null, assume the user is referencing the current date (right now)
+} else {
+eDate = new Date(lookupDateTime); // If not null, set the epic date to the timestamp given
 }
 
-// =============================================================================
-// gatherDayInfo
-//
-// ***SUBJECT TO CHANGE: A function called by enableTimer that gets updated date
-// information. The day variables (today, hour, minute, second) will likely be
-// removed. todayObj was a day-map from [DEPRECATED]-content.js that controlled
-// the displaying of date information on the extension.
-//
-// Arguments:
-//
-// gatherDayInfo takes no arguments
-// =============================================================================
+console.log ("Looking for the day/period for " + eDate);
 
-function gatherDayInfo() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    currentDate = new Date();
-    today = currentDate.getDay();
-    hour = currentDate.getHours();
-    minute = currentDate.getMinutes();
-    second = currentDate.getSeconds();
-    var todayObj = dayMap[today];
-    printDay(todayObj);
+
+let match = calendar.getPeriodByDateAndTime(eDate);
+
+if (match === null) {
+
+    // if match is null, then there is no match against the current date/time,
+    // which likely means that the date/time is outside the current school year
+    console.log ("No match on " + eDate)
+
+    // Display "not in school" information on extension
+    context.font = textPos.nameSize;
+    context.fillText("Summer | Free", textPos.x, textPos.y) // There is only 1 year worth of data as of now, so this CANNOT display a "time left in period" number
+
+} 
+else if (match.pObj === null) {
+
+    // if match.pObj is null, then there was a day match, but there are no
+    // periods for that day, as would be the case on a weekend or a holiday
+    let dObj = match.dObj;
+    console.log (
+        dObj.dayName + ", " +
+        dObj.printDate + " is a " +
+        dObj.dayType + " and has no periods"
+    );
+
+    // Display "during school year but not a day that has school" --> weekends and holidays
+    context.font = textPos.nameSize;
+    context.fillText(dObj.dayType + " | Free", textPos.x, textPos.y)
+
+} 
+else {
+
+    // Found a match on both day and period.
+    let dObj = match.dObj; // day object
+    let pObj = match.pObj; // period object
+
+    // Print day information
+    console.log (
+        dObj.dayName + ", " +
+        dObj.printDate + " is a " +
+        dObj.dayType
+    );
+
+    // Print period information
+    console.log (
+        "Period " + pObj.name +
+        " starts at " + pObj.startSTime +
+        " and ends at " + pObj.endSTime
+    );
+
+    // Print class information
+    if (pObj.period >= 0) {
+
+        let cObj = pObj.classInfoObject;
+        console.log (
+        "The class in this period is " + cObj.className +
+        ", taught by " + cObj.teacher  +
+        ", in room " + cObj.room
+        );
+
+        // Display "during an active period" --> this only includes real periods, NOT passing periods
+        // Test if cObj is null, return special case "No class", else return cObj.className
+        let className = (cObj === null) ? "No class" : cObj.className
+
+        context.font = textPos.nameSize;
+        context.fillText(className + " | " + pObj.name, textPos.x, textPos.y)
+
+    } 
+    else {
+        console.log ("There is no class during this period");
+
+    } // if (pOjb.period >= 0) ... else
+
+    // Print time left in period
+    let timeLeft = calendar.getTimeRemainingInPeriod(eDate, match.pObj);
+    console.log (
+        "Time remaining in the period is " +
+        CalendarHHMMSSAsString(timeLeft.hDelta, timeLeft.mDelta, timeLeft.sDelta)
+    );
+
+    // Print time remaining for any applicable period
+    context.font = textPos.timeSize;
+    context.fillText(CalendarHHMMSSAsString(timeLeft.hDelta, timeLeft.mDelta, timeLeft.sDelta), textPos.x - textPos.xOffset, textPos.y + textPos.yOffset)
 }
+
+  // if (match === null) ... else if ... else
+ // if (_enableExampleCode)
+ 
