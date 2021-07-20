65d48051280bdad22fca326942f70386807f5f81
Jonathan Uhler <jonathan@uhler.com>
2021-07-20 09:57:26 -0700

    3.1.1	7/20/21		Changes in this version 							-Cleaned up documentation

--- settings.js
+++ settings.js
@@ -1,10 +1,13 @@
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 // settings.js
+// Period-Countdown
 //
-// Coordination of information from Calendar.js and display.js to allow the user to change settings in a web-frontend
+// Manages the user using the settings and utilities menus
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
 "use strict";
 
-// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 // Copyright 2020 Jonathan Uhler and Mike Uhler
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,158 +27,125 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
-// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
 
 // Canvas and context properties
 var canvas = document.getElementById('Periods');
 var context = canvas.getContext('2d');
 
-let SettingsVersion = "1.0.0";
 
-// Revision History
-//
-//  version    date                     Change
-//  ------- ----------  --------------------------------------------------------
-//  1.0.0   1/24/2021   -First working version of settings.js
-//
-//  2.0.0   1/24/2021   Changes in this version:
-//                          -File I/O replaced with cookies
-//                          -Default classes set to be "None" for all 7 periods
-//                          -Issues with cookies fixed
-//                          -Textboxes will now display default values
-//
-// 3.0.0    1/26/2021   Changes in this version:
-//                          -Minimized and cleaned up code for settings
-//                          -Rewrote HTML settings elements to minimize code
-//                          -Added in functions for utilities
-//                          -Added support for utilities
-//                          -Condensed utilities and classes into 2 divs
-// 3.1.0    1/26/2021   Changes in this version:
-//                          -A simple fade in/out animation has been added to all
-//                           elements to improve user experience
-
-// Version information
-this.Version = SettingsVersion
-console.log("Settings v" + this.Version);
-
-
-// ===========================================================================
+// ====================================================================================================
 // function SettingsMessage
 //
-// Function to emit a message, with optional arguments, which are separated
-// by ", "
+// Displays a message with comma separated elements
 //
 // Arguments--
 //
-// msg:        Message
+// msg:     main message to be displayed
 //
-// args:       Optional list of arguments to output
+// args:    list of extra arguments to attatch
 //
 // Returns--
 //
 // None
 //
 function SettingsMessage(msg, ...args) {
-
-    let message = "Settings Message: " + msg
+    // Create the message
+    let message = "Settings Message: " + msg;
     if (args.length > 0) {
-      message += " " + args.join(", ")
+      message += " " + args.join(", ");
     }
+    // Display the message
+    console.log(message);
+}
+// end function SettingsMessage
 
-    console.log(message)
 
-} // end function SettingsMessage
-
-
-// ===========================================================================
+// ====================================================================================================
 // function fade
 //
-// A function to fade an element
+// Fades an html element
 //
 // Arguments--
 //
-// element:         the name of the HTML element to fade
+// element: the name of the HTML element to fade
 //
 // Returns--
 //
 // None
 //
 function fade(element) {
-    var op = 1 // initial opacity
+    var op = 1; // initial opacity
     var timer = setInterval(function () {
         if (op <= 0.1) {
             clearInterval(timer);
-            document.getElementById(element).style.display = 'none'
+            document.getElementById(element).style.display = 'none';
         }
 
-        document.getElementById(element).style.opacity = op
-        document.getElementById(element).style.filter = 'alpha(opacity=' + op * 100 + ")"
+        document.getElementById(element).style.opacity = op;
+        document.getElementById(element).style.filter = 'alpha(opacity=' + op * 100 + ")";
 
-        op -= op * 0.5
+        op -= op * 0.5;
 
     }, 30);
-} // end: function fade
+}
+// end: function fade
 
 
-// ===========================================================================
+// ====================================================================================================
 // function unfade
 //
-// A function to unfade an element
+// Unfades an html element
 //
 // Arguments--
 //
-// element:         the name of the HTML element to unfade
+// element: the name of the HTML element to unfade
 //
 // Returns--
 //
 // None
 //
 function unfade(element) {
-    var op = 0.1 // initial opacity
-    document.getElementById(element).style.display = 'block'
+    var op = 0.1; // initial opacity
+    document.getElementById(element).style.display = 'block';
     var timer = setInterval(function () {
         if (op >= 1) {
             clearInterval(timer);
         }
 
-        document.getElementById(element).style.opacity = op
-        document.getElementById(element).style.filter = 'alpha(opacity=' + op * 100 + ")"
+        document.getElementById(element).style.opacity = op;
+        document.getElementById(element).style.filter = 'alpha(opacity=' + op * 100 + ")";
 
-        op += op * 0.5
+        op += op * 0.5;
 
     }, 50);
 } // end: function fade
 
 
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
-// ---------------------
-// SETTINGS BUTTON
-// ---------------------
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+// MARK: Settings button
 
-
-var hideElems = ["classes", "submitPeriods", "utils"]
-var userClassDefaults = []
+var hideElems = ["classes", "submitPeriods", "utils"];
+var userClassDefaults = [];
 // HTML class textbox element IDs
-var textIDs = ["P1Text", "P2Text", "P3Text", "P4Text", "P5Text", "P6Text", "P7Text"]
+var textIDs = ["P1Text", "P2Text", "P3Text", "P4Text", "P5Text", "P6Text", "P7Text"];
 // Settings button icon
-document.getElementById("goToSettings").addEventListener("click", openSettingsTab)
+document.getElementById("goToSettings").addEventListener("click", openSettingsTab);
 
 // Make the boxes start hidden until the settings button is pressed
 for (var i = 0; i < hideElems.length; i++) {
-    document.getElementById(hideElems[i]).style.display = 'none'
+    document.getElementById(hideElems[i]).style.display = 'none';
 }
 
 // Button to save the periods
-var submitButton = document.getElementById('submitPeriods')
-submitButton.addEventListener("click", saveUserDefaults, false)
+var submitButton = document.getElementById('submitPeriods');
+submitButton.addEventListener("click", saveUserDefaults, false);
 
 
-// =============================================================================
-// function saveUserDefaults()
+// ====================================================================================================
+// function saveUserDefaults
 //
-// A function that is controlled by the "Save Classes" button and saves all
-// choices into an array
+// Saves class names
 //
 // Arguments--
 //
@@ -186,105 +156,102 @@ submitButton.addEventListener("click", saveUserDefaults, false)
 // None
 //
 function saveUserDefaults() {
-
     // Clear all previous choices
-    userClassDefaults = []
+    userClassDefaults = [];
 
     // Save each of the strings in the textboxes to an array
     for (var i = 0; i <= textIDs.length - 1; i++) {
-
-        var classChoice = document.getElementById(textIDs[i]).value
+        // Get the value
+        var classChoice = document.getElementById(textIDs[i]).value;
+        // Check for free periods
         if (classChoice.toLowerCase() === "none" || classChoice.toLowerCase() === "n/a" || classChoice.toLowerCase() === "free") {
-            userClassDefaults.push(null)
+            userClassDefaults.push(null);
         }
+        // Otherwise push the name
         else {
-            userClassDefaults.push(classChoice)
+            userClassDefaults.push(classChoice);
         }
-
     }
 
-    // Debug: display all the choices
-    SettingsMessage(`New Classes:`, userClassDefaults)
+    // Display all the choices
+    SettingsMessage(`New Classes:`, userClassDefaults);
 
     // Write the choices to a file to be read by MVHS.js
-    removeCookie("periods")
-    createCookie("periods", userClassDefaults)
+    removeCookie("periods");
+    createCookie("periods", userClassDefaults);
+}
+// end: function saveUserDefaults
 
-} // end: function saveUserDefaults
 
-
-// =============================================================================
+// ====================================================================================================
 // function removeCookie
 //
-// A function to remove an existing cookie by setting its expiration date to the
-// past
+// Removes an existing cookie
 //
 // Arguments--
 //
-// name:            the name of the cookie to remove
+// name:    the name of the cookie to remove
 //
 // Returns--
 //
 // None
 //
 function removeCookie(name) {
+    var removeStatement = name + "= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = removeStatement;
+}
+// end: function removeCookie
 
-    var removeStatement = name + "= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
-    document.cookie = removeStatement
 
-} // end: function removeCookie
-
-
-// =============================================================================
+// ====================================================================================================
 // function createCookie
 //
-// A function to create a new cookie
+// Creates a new cookie
 //
 // Arguments--
 //
-// name:            the name of the new cookie
+// name:    the name of the new cookie
 //
-// value:           the value that the cookie should have
+// value:   the value that the cookie should have
 //
 // Returns--
 //
 // None
 //
 function createCookie(name, value) {
+    var newCookie = name + "=" + value;
+    document.cookie = newCookie;
 
-    var newCookie = name + "=" + value
-    document.cookie = newCookie
-
-    SettingsMessage("New cookie data:", document.cookie)
-
-} // end: createCookie
+    SettingsMessage("New cookie data:", document.cookie);
+}
+// end: createCookie
 
 
-// =============================================================================
-// function openSettingsTab()
+// ====================================================================================================
+// function openSettingsTab
 //
 // A function to show and hide the text boxes where class names can be entered
 //
 // Arguments--
 //
-// idArray:     the array of class div ids
+// None
 //
 // Returns--
 //
 // None
 //
 function openSettingsTab() {
-
-    var period = document.getElementById("classes")
+    // Get the classes div
+    var period = document.getElementById("classes");
 
     // Read in cookie data
     try {
-        var cookieData = document.cookie
-        var periodsFromCookie = cookieData.split("=")
-        periodsFromCookie = periodsFromCookie[1].split(",")
+        var cookieData = document.cookie;
+        var periodsFromCookie = cookieData.split("=");
+        periodsFromCookie = periodsFromCookie[1].split(",");
     }
-    catch {
-        periodsFromCookie = ["None", "None", "None", "None", "None", "None", "None"]
+    catch { // Cookie was empty/didn't exist, default to all none
+        periodsFromCookie = ["None", "None", "None", "None", "None", "None", "None"];
     }
 
     for (var j = 0; j <= periodsFromCookie.length - 1; j++) {
@@ -294,44 +261,50 @@ function openSettingsTab() {
 
     // Show or hide the 7 textboxes
     if (getComputedStyle(period).display === 'none') {
-        fade("goToUtilities")
-        unfade("submitPeriods")
-        unfade("classes")
+        fade("goToUtilities");
+        unfade("submitPeriods");
+        unfade("classes");
     }
     else {
-        unfade("goToUtilities")
-        fade("submitPeriods")
-        fade("classes")
+        unfade("goToUtilities");
+        fade("submitPeriods");
+        fade("classes");
     }
-
-} // end: function openSettingsTab
-
-
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
-// ---------------------
-// UTILITIES BUTTON
-// ---------------------
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+}
+// end: function openSettingsTab
 
 
-document.getElementById("goToUtilities").addEventListener("click", openUtilitiesTab)
+// MARK: Utilities button
+
+document.getElementById("goToUtilities").addEventListener("click", openUtilitiesTab);
 
 
+// ====================================================================================================
+// function openUtilitiesTab
+//
+// Opens the utilities menu
+//
+// Arguments--
+//
+// None
+//
+// Returns--
+//
+// None
+//
 function openUtilitiesTab() {
-
-    var util = document.getElementById("utils")
+    var util = document.getElementById("utils");
 
     // Show or hide the utilities icons
     if (getComputedStyle(util).display === 'none') {
-        fade("goToSettings")
-        fade("classes")
-        unfade("utils")
+        fade("goToSettings");
+        fade("classes");
+        unfade("utils");
     }
     else {
-        unfade("goToSettings")
-        fade("classes")
-        fade("utils")
+        unfade("goToSettings");
+        fade("classes");
+        fade("utils");
     }
-
 }
-
+// end: function openUtilitiesTab
